413a9052a8ae850c2ccc02de13ea34c3585c4435
DevExpressExampleBot <fakeEmail>
2023-03-23 15:24:31 +0400

    Source auto update [skip ci]

--- VB/Report_ParametertsPanel/Form1.Designer.vb
+++ VB/Report_ParametertsPanel/Form1.Designer.vb
@@ -1,57 +1,54 @@
-﻿Imports Microsoft.VisualBasic
-Imports System
 Namespace Report_ParametertsPanel
-	Partial Public Class Form1
-		''' <summary>
-		''' Required designer variable.
-		''' </summary>
-		Private components As System.ComponentModel.IContainer = Nothing
 
-		''' <summary>
-		''' Clean up any resources being used.
-		''' </summary>
-		''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-		Protected Overrides Sub Dispose(ByVal disposing As Boolean)
-			If disposing AndAlso (components IsNot Nothing) Then
-				components.Dispose()
-			End If
-			MyBase.Dispose(disposing)
-		End Sub
+    Partial Class Form1
 
-		#Region "Windows Form Designer generated code"
+        ''' <summary>
+        ''' Required designer variable.
+        ''' </summary>
+        Private components As System.ComponentModel.IContainer = Nothing
 
-		''' <summary>
-		''' Required method for Designer support - do not modify
-		''' the contents of this method with the code editor.
-		''' </summary>
-		Private Sub InitializeComponent()
-			Me.simpleButton1 = New DevExpress.XtraEditors.SimpleButton()
-			Me.SuspendLayout()
-			' 
-			' simpleButton1
-			' 
-			Me.simpleButton1.Location = New System.Drawing.Point(71, 75)
-			Me.simpleButton1.Name = "simpleButton1"
-			Me.simpleButton1.Size = New System.Drawing.Size(141, 95)
-			Me.simpleButton1.TabIndex = 0
-			Me.simpleButton1.Text = "Show Print Preview"
-'			Me.simpleButton1.Click += New System.EventHandler(Me.simpleButton1_Click);
-			' 
-			' Form1
-			' 
-			Me.AutoScaleDimensions = New System.Drawing.SizeF(6F, 13F)
-			Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-			Me.ClientSize = New System.Drawing.Size(284, 264)
-			Me.Controls.Add(Me.simpleButton1)
-			Me.Name = "Form1"
-			Me.Text = "Form1"
-			Me.ResumeLayout(False)
+        ''' <summary>
+        ''' Clean up any resources being used.
+        ''' </summary>
+        ''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+            If disposing AndAlso (Me.components IsNot Nothing) Then
+                Me.components.Dispose()
+            End If
 
-		End Sub
+            MyBase.Dispose(disposing)
+        End Sub
 
-		#End Region
+'#Region "Windows Form Designer generated code"
+        ''' <summary>
+        ''' Required method for Designer support - do not modify
+        ''' the contents of this method with the code editor.
+        ''' </summary>
+        Private Sub InitializeComponent()
+            Me.simpleButton1 = New DevExpress.XtraEditors.SimpleButton()
+            Me.SuspendLayout()
+            ' 
+            ' simpleButton1
+            ' 
+            Me.simpleButton1.Location = New System.Drawing.Point(71, 75)
+            Me.simpleButton1.Name = "simpleButton1"
+            Me.simpleButton1.Size = New System.Drawing.Size(141, 95)
+            Me.simpleButton1.TabIndex = 0
+            Me.simpleButton1.Text = "Show Print Preview"
+            AddHandler Me.simpleButton1.Click, New System.EventHandler(AddressOf Me.simpleButton1_Click)
+            ' 
+            ' Form1
+            ' 
+            Me.AutoScaleDimensions = New System.Drawing.SizeF(6F, 13F)
+            Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+            Me.ClientSize = New System.Drawing.Size(284, 264)
+            Me.Controls.Add(Me.simpleButton1)
+            Me.Name = "Form1"
+            Me.Text = "Form1"
+            Me.ResumeLayout(False)
+        End Sub
 
-		Private WithEvents simpleButton1 As DevExpress.XtraEditors.SimpleButton
-	End Class
+'#End Region
+        Private simpleButton1 As DevExpress.XtraEditors.SimpleButton
+    End Class
 End Namespace
-
